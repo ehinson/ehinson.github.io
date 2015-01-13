576f65a844c53f29bab9f69baceec48d2b430cbc
Erin Hinson <hinson.erin@gmail.com>
2015-01-12 23:32:41 -0500

    sticky scrolling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+ $(window).scroll(function(){
+    if ($(this).scrollTop() > 600) {
+      $('.subhead h2').addClass("affix");
+      
+    }
+    else{
+      $('.subhead h2').removeClass("affix");
+    }
+  });
+
+ $(window).scroll(function(){
+    if ($(this).scrollTop() > 750) {
+      $('.subhead').addClass("fix-div");
+
+    }
+    else{
+      $('.subhead').removeClass("fix-div");
+    }
+ });
+
+
 $(document).ready(function(){
   $("#p1").mouseenter(function(){
     $("#p2").css("display", "inline-block");
@@ -5,4 +26,10 @@ $(document).ready(function(){
   $("#p1").mouseleave(function(){
     $("#p2").css("display", "none");
   });
+
 });
+
+
+
+
+
