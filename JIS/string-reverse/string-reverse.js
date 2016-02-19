735fbe7df53f34b413ccc69f3772cb0236acf598
Erin Hinson <erin.hinson@Erin-Hinsonss-MacBook-Pro.local>
2016-02-19 12:29:15 -0500

    Javascript is Sexy examples Week 1-2

---
+++ JIS/string-reverse/string-reverse.js
@@ -0,0 +1,7 @@
+
+
+function stringReverse(){
+  var text = document.getElementById("userInput1").value;
+  text  = text.split("").reverse().join("");
+  document.getElementById("userOutput1").innerHTML = text;
+}
