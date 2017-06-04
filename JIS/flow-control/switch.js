bab2b7edac501409070867ca3b511999d9fbd9ca
Erin Hinson <erin.hinson@xcelc02q33v.attlocal.net>
2017-06-04 13:40:55 -0400

    New Site

--- JIS/flow-control/switch.js
+++
@@ -1,8 +0,0 @@
-var userInput = document.GetElementById("userInput1").value;
-
-function userSwitch(){
-  switch(input){
-    case :
-    break;
-  }
-}
--- JIS/string-reverse/string-reverse.js
+++
@@ -1,6 +0,0 @@
-
-function stringReverse(){
-  var text = document.getElementById("userInput1").value;
-  text  = text.split("").reverse().join("");
-  document.getElementById("userOutput1").innerHTML = text;
-}
