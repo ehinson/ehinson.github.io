c5734050056050e7ee0bae8c18802b42937d8598
ehinson <erinqhinson@gmail.com>
2019-12-15 11:36:07 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6f7922353fdaeabf732ebe878524268.js"
+  "/precache-manifest.68e09e73c7c625f9045b9d897dc8892d.js"
 );
 
 self.addEventListener('message', (event) => {
